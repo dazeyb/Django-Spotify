045c023e7f7481eb793424971331ec346499a1ea
DaltonHart <daltonvaren@gmail.com>
2021-05-10 12:12:27 -0700

    added database connection for artist list

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# this will import the Artist Model
+from .models import Artist
+
 
 # Register your models here.
+
+admin.site.register(Artist)
--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,11 @@ from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 
+# import models
+from .models import Artist
 
 # Create your views here.
+
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -13,27 +16,18 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-# fake database model 
-
-class Artist:
-    def __init__(self, name, image, bio):
-        self.name = name
-        self.image = image 
-        self.bio = bio
-
-artists = [
-    Artist("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29","Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),
-    Artist("Panic! At The Disco", "https://i.scdn.co/image/58518a04cdd1f20a24cf0545838f3a7b775f8080", "Welcome 👋 The Amazing Beebo was working on a new bio but now he's too busy taking over the world."),
-    Artist("Joji", "https://i.scdn.co/image/7bc3bb57c6977b18d8afe7d02adaeed4c31810df","Joji is one of the most enthralling artists of the digital age. New album Nectar arrives as an eagerly anticipated follow-up to Joji's RIAA Gold-certified first full-length album BALLADS 1, which topped the Billboard R&B / Hip-Hop Charts and has amassed 3.6B+ streams to date."),
-    Artist("Metallica","https://i.scdn.co/image/ab67706c0000da84eb6bb372a485d26fd32d1922", "Metallica formed in 1981 by drummer Lars Ulrich and guitarist and vocalist James Hetfield and has become one of the most influential and commercially successful rock bands in history, having sold 110 million albums worldwide while playing to millions of fans on literally all seven continents.")
-]
-
 
 class ArtistList(TemplateView):
     template_name = "artist_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["artists"] = artists
+        name_query = self.request.GET.get("name")
+        if name_query != None:
+            context["artists"] = Artist.objects.filter(
+                name__icontains=name_query)
+            context["header"] = f"Searching for {name_query}"
+        else:
+            context["artists"] = Artist.objects.all()
+            context["header"] = "Trending Artists"
         return context
-
