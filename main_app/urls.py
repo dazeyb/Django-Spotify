00d78a25a699a6fe87ff79b0878992707855f6b8
DaltonHart <daltonvaren@gmail.com>
2021-05-10 15:15:03 -0700

    adds artist delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('artists/new', views.ArtistCreate.as_view(), name="artist_create"),
     path('artists/<int:pk>/', views.ArtistDetail.as_view(), name="artist_detail"),
     path('artists/<int:pk>/update',
-         views.ArtistUpdate.as_view(), name="artist_update")
+         views.ArtistUpdate.as_view(), name="artist_update"),
+    path('artists/<int:pk>/delete',
+         views.ArtistDelete.as_view(), name="artist_delete")
 ]
